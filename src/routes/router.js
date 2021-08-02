880304590edb020c01262bccebbfd784315a41da
mohammad <mawebcoder@gmail.com>
2021-08-02 10:02:55 +0430

    set router system in project

---
+++ src/routes/router.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import VueRouter from "vue-router";
+import routes from "./routes";
+
+Vue.use(VueRouter)
+
+
+const router = new VueRouter({
+    routes,
+    mode: 'history',
+    scrollBehavior() {
+        return {
+            x: 0,
+            y: 0,
+            behavior: 'smooth'
+        }
+    }
+})
+
+export default router;
