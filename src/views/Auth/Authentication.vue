2eb54f2a772c713d4056f4941fa9837789a1bdca
mohammad <mawebcoder@gmail.com>
2021-08-05 12:05:28 +0430

    add auth directories

---
+++ src/views/Auth/Authentication.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+
+</div>
+</template>
+
+<script>
+export default {
+  name: "Authentication"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Auth/VerifyCode.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "VerifyCode"
+}
+</script>
+
+<style scoped>
+
+</style>
