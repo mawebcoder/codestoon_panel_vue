1016f76670b84c27657fc6925c23916ac3ab8afc
mohammad <mawebcoder@gmail.com>
2021-08-05 12:39:05 +0430

    end format of the frontend for authentication page

--- src/views/Auth/AuthenticationPage.vue
+++ src/views/Auth/AuthenticationPage.vue
@@ -1,15 +1,56 @@
 <template>
   <div>
-    <span style="color: red">auth page</span>
+    <main dir="rtl">
+      <div class="card-container">
+        <md-card>
+          <md-card-header>
+            <div class="md-title">ورود به پنل مدیریت</div>
+          </md-card-header>
+
+          <md-card-content>
+            <md-field>
+              <md-input placeholder="شماره تلفن خود را وارد کنید..." v-model="mobile"></md-input>
+              <md-icon>phone</md-icon>
+            </md-field>
+          </md-card-content>
+          <md-card-actions>
+            <md-button class="md-raised md-primary">
+
+              ورود
+
+              <md-icon style="margin-right: 10px">login</md-icon>
+            </md-button>
+          </md-card-actions>
+
+        </md-card>
+      </div>
+
+    </main>
+
   </div>
 </template>
 
 <script>
 export default {
-  name: "AuthenticationPage"
+  name: "AuthenticationPage",
+  data() {
+    return {
+      mobile: ''
+    }
+  }
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+.card-container {
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  width: 100%;
 
+  > div {
+    width: 350px;
+    margin-top: 50px;
+  }
+}
 </style>
