ccbcdebbe7b3e5ac900ba7c59611bae51dcc4f03
mohammad <mawebcoder@gmail.com>
2021-08-20 23:45:34 +0430

    add meta and shortdescription and description and slug field to course category

--- src/views/Course/Category/create.vue
+++ src/views/Course/Category/create.vue
@@ -10,6 +10,47 @@
 
       <md-input placeholder="نام دسته بندی به انگلیسی..." v-model="en_name"></md-input>
     </md-field>
+
+    <md-field>
+      <md-input placeholder="اسلاگ..." v-model="slug"></md-input>
+    </md-field>
+    <div class="form-group">
+      <md-field>
+        <label>اطلاعات متا...</label>
+        <md-textarea v-model="meta"></md-textarea>
+      </md-field>
+    </div>
+
+    <div class="form-group">
+      <md-field>
+        <label>توضیحات کوتاه...</label>
+        <md-textarea v-model="short_description"></md-textarea>
+      </md-field>
+    </div>
+
+    <label>
+      توضیحات کامل :
+    </label>
+    <editor
+        v-model="description"
+        api-key="214siyv4hmul2xqvhali31m8ox5kxrskd3g1k5b6an2ft09l"
+        :init="{
+                menubar: true,
+                language:'fa',
+                plugins: [
+                'advlist directionality codesample autolink link image lists charmap print preview hr anchor pagebreak',
+                'searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking',
+                'table emoticons template paste help'
+                 ],
+                width: 1000,
+                height: 300,
+                images_upload_url:$store.state.baseUrl+'/api/articles/upload-article-images',
+                // image_upload_base_path:$store.state.baseUrl,
+                toolbar: 'undo redo | styleselect | codesample | bold italic | alignleft aligncenter alignright alignjustify | ' +
+                'bullist numlist outdent indent | link image | print preview media fullpage | ' +
+                'forecolor backcolor emoticons | help | rtl ltr',
+                 }"
+    />
     <multiselect selectedLabel=" " selectLabel="انتخاب " deselectLabel="حذف" v-model="parentObject"
                  :options="parentArray" :close-on-select="true"
                  :clear-on-select="false"
@@ -32,6 +73,7 @@
 </template>
 
 <script>
+import Editor from '@tinymce/tinymce-vue'
 import HelperClass from "../../../services/HelperClass";
 import CourseCategoryService from "../../../services/Course/CourseCategoryService";
 import Multiselect from 'vue-multiselect'
@@ -47,6 +89,9 @@ export default {
       parentObject: {name: 'بدون دسته والد', value: 0},
       status: false,
       fa_name: '',
+      meta:"",
+      slug:'',
+      short_description:'',
       en_name: '',
       parentArray: [
         {name: 'بدون دسته والد', value: 0},
@@ -84,6 +129,12 @@ export default {
 
       let data = new FormData();
       data.append('name', this.fa_name);
+      data.append('short_description',this.short_description);
+      data.append('description',this.description);
+      data.append('meta',this.meta);
+
+      this.slug.trim().length?
+          data.append('slug',this.slug):'';
       this.en_name.trim().length ?
           data.append('en_name', this.en_name) :
           '';
@@ -132,7 +183,8 @@ export default {
   },
   components: {
     Multiselect,
-    UploadImage
+    UploadImage,
+    Editor
   },
   mounted() {
   }
