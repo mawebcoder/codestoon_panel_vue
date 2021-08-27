84dae62698fe085d20ac4484bfc90cdfe46f0910
mohammad <mawebcoder@gmail.com>
2021-08-27 17:54:25 +0430

    store course tags

--- src/services/Course/CourseTagService.js
+++ src/services/Course/CourseTagService.js
@@ -1,11 +1,14 @@
 import HttpVerbs from "../HttpVerbs";
 class CourseTagService{
-    constructor(courseCategoryBaseUrl) {
-        this.courseCategoryBaseUrl=courseCategoryBaseUrl;
+    constructor(courseTagBaseUrl) {
+        this.courseTagBaseUrl=courseTagBaseUrl;
     }
 
     getTags(){
-        return HttpVerbs.getRequest(`${this.courseCategoryBaseUrl}/select/box`)
+        return HttpVerbs.getRequest(`${this.courseTagBaseUrl}/select/box`)
+    }
+    store(data){
+        return HttpVerbs.postRequest(`${this.courseTagBaseUrl}`,data)
     }
 }
 export default new CourseTagService('courses/tags');
--- src/views/Course/Tag/create.vue
+++ src/views/Course/Tag/create.vue
@@ -1,12 +1,94 @@
 <template>
-<div>
-  create course tag
-</div>
+  <div>
+
+    <md-field>
+
+      <md-input placeholder="نام تگ به فارسی..." v-model="fa_name"></md-input>
+    </md-field>
+
+    <md-field>
+
+      <md-input placeholder="نام تگ به انگلیسی..." v-model="en_name"></md-input>
+    </md-field>
+
+    <md-field>
+
+      <md-input placeholder="اسلاگ..." v-model="slug"></md-input>
+    </md-field>
+
+    <md-field>
+
+      <md-input placeholder="توضیحات متا..." v-model="meta"></md-input>
+    </md-field>
+
+    <md-field>
+
+      <md-textarea placeholder="توضیحات کوتاه..." v-model="short_description"></md-textarea>
+    </md-field>
+
+
+    <div dir="ltr">
+      <md-switch v-model="status"></md-switch>
+    </div>
+
+    <md-button @click="submit" class="md-raised md-primary">ثبت</md-button>
+
+  </div>
 </template>
 
 <script>
+import CourseTagService from "../../../services/Course/CourseTagService";
+import HelperClass from "../../../services/HelperClass";
+
 export default {
-  name: "create"
+  name: "Create",
+  data() {
+    return {
+      status: false,
+      fa_name: '',
+      en_name: '',
+      slug: "",
+      meta: "",
+      short_description: '',
+    }
+  },
+  methods: {
+    getData() {
+      let formData = new FormData();
+      formData.append('fa_title', this.fa_name);
+      formData.append('en_title', this.en_name);
+      formData.append('status', this.status ? 1 : 0);
+      formData.append('slug', this.slug );
+      formData.append('meta', this.meta);
+
+      this.short_description.trim().length?
+          formData.append('short_description',this.short_description):'';
+      return formData;
+    },
+    makeEmptyValues() {
+      this.fa_name = '';
+      this.en_name = '';
+      this.status = false;
+      this.meta='';
+      this.short_description='';
+      this.slug='';
+    },
+    submit() {
+      this.$store.state.loader = true;
+      let data = this.getData();
+
+      CourseTagService.store(data)
+          .then(() => {
+            HelperClass.showSuccess(this.$noty)
+            this.makeEmptyValues();
+          }).catch(error => {
+        HelperClass.showErrors(error, this.$noty)
+      })
+
+    }
+  },
+  mounted() {
+  }
 }
 </script>
 
