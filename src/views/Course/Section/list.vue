a499e977aa3e481bd91a3bca83d6c3383790b793
mohammad <mawebcoder@gmail.com>
2021-10-20 21:58:17 +0330

    get course sections done

--- src/views/Course/Section/list.vue
+++ src/views/Course/Section/list.vue
@@ -1,12 +1,53 @@
 <template>
-<div>
-  course section list
-</div>
+  <div>
+
+    <DataTable :items="rows" :uri="uri" :editUrlName="edit_url_name" :delete-url="delete_uri" :columns="columns">
+    </DataTable>
+
+  </div>
 </template>
 
 <script>
+const DataTable = () => import('../../../components/DataTable')
+
 export default {
-  name: "list"
+  name: "list",
+  data() {
+    return {
+      rows: ['id', 'title', 'en_title', 'status', 'courseTitle'],
+      edit_url_name: 'edit-article-tag',
+      delete_uri: 'courses/sections',
+      uri: 'courses/sections',
+      columns: [
+        {
+          field: 'id',
+          label: 'شناسه',
+        },
+        {
+          field: 'title',
+          label: 'عنوان فارسی'
+        },
+        {
+          field: 'en_title',
+          label: 'عنوان لاتین'
+        },
+        {
+          field: 'status',
+          label: 'وضعیت'
+        },
+        {
+          field: 'courseTitle',
+          label: 'عنوان دوره'
+        }
+      ],
+    }
+  },
+  components: {
+    DataTable
+  },
+  mounted() {
+  }
+
 }
 </script>
 
