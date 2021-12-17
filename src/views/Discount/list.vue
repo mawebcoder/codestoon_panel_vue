7d36c2ac70d695ff6da07c31faad7470e6b6cb55
mohammad <mawebcoder@gmail.com>
2021-12-17 20:24:14 +0330

    add disocunt search

--- src/views/Discount/list.vue
+++ src/views/Discount/list.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
 
-    <DataTable :items="rows" :uri="uri" :editUrlName="edit_url_name" :delete-url="delete_uri" :columns="columns">
+    <DataTable  server-search-route="discounts/search-items" :items="rows"  :editUrlName="edit_url_name"
+                :delete-url="delete_uri" :columns="columns">
     </DataTable>
 
   </div>
