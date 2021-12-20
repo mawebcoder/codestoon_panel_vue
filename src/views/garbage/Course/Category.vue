b15cdc0d36b9dbb3e959439e0361d676d911b465
mohammad <mawebcoder@gmail.com>
2021-12-20 21:55:43 +0330

    add search to trash course Categories

--- src/views/garbage/Course/Category.vue
+++ src/views/garbage/Course/Category.vue
@@ -1,12 +1,47 @@
 <template>
-<div>
-  course category
-</div>
+  <div>
+
+    <DataTable :force-delete="true" restore-url="courses/categories/restore" force-delete-url="courses/categories/force/delete"
+               :showRestore="true" :isTrash="true"
+               server-search-route="courses/categories/search-items?trash=true" :items="rows"
+               :editUrlName="edit_url_name"
+               :delete-url="delete_uri" :show-edit="false" :columns="columns">
+    </DataTable>
+
+  </div>
 </template>
 
 <script>
+const DataTable = () => import('../../../components/DataTable')
+
 export default {
-  name: "Category"
+  name: "list",
+  data() {
+    return {
+      rows: [
+        'id',
+        'name'
+      ],
+      edit_url_name: 'factor-course-edit',
+      delete_uri: 'factors',
+      columns: [
+        {
+          field: 'id',
+          label: 'شناسه',
+        },
+        {
+          field: 'name',
+          label: 'عنوان دوره'
+        },
+      ]
+    }
+  },
+  components: {
+    DataTable
+  },
+  mounted() {
+  }
+
 }
 </script>
 
