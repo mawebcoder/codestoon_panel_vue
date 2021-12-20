e59142eb7368055050a30e53b56971ab2f9693ce
mohammad <mawebcoder@gmail.com>
2021-12-20 21:47:00 +0330

    add search to course trash searches

--- src/routes/Recyclebin/Course.js
+++ src/routes/Recyclebin/Course.js
@@ -1,3 +1,5 @@
+import Auth from "../../services/Auth/Auth";
+
 export default [
     {
         path:'courses',
@@ -6,23 +8,36 @@ export default [
             {
                 path:'/',
                 component:()=>import('../../views/garbage/Course/Course'),
-                name:'course-garbage'
+                name:'course-garbage',
+                beforeEnter: (to, from, next) => {
+                    Auth.checkCanAccessThisRoute(next, 'user.update', 'سطل آشغال دوره ها')
+                }
             },
             {
                 path:'categories',
                 component:()=>import('../../views/garbage/Course/Category'),
-                name:'course-category-garbage'
+                name:'course-category-garbage',
+                beforeEnter: (to, from, next) => {
+                    Auth.checkCanAccessThisRoute(next, 'user.update', 'سطل آشغال دسته بندی دوره ها')
+                }
             },
+
             {
                 path:'sections',
                 component:()=>import('../../views/garbage/Course/Section'),
-                name: 'course-section-garbage'
+                name: 'course-section-garbage',
+                beforeEnter: (to, from, next) => {
+                    Auth.checkCanAccessThisRoute(next, 'user.update', 'سطل آشغال فصل های دوره ها')
+                }
 
             },
             {
                 path:'videos',
                 component:()=>import('../../views/garbage/Course/Video'),
-                name: 'course-videos-garbage'
+                name: 'course-videos-garbage',
+                beforeEnter: (to, from, next) => {
+                    Auth.checkCanAccessThisRoute(next, 'user.update', 'سطل آشغال ویدیوها')
+                }
 
             }
         ]
--- src/views/garbage/Course/Course.vue
+++ src/views/garbage/Course/Course.vue
@@ -1,12 +1,47 @@
 <template>
-<div>
-  course garbage
-</div>
+  <div>
+
+    <DataTable :force-delete="true" restore-url="courses/restore" force-delete-url="courses/force/delete"
+               :showRestore="true" :isTrash="true"
+               server-search-route="courses/search-items?trash=true" :items="rows"
+               :editUrlName="edit_url_name"
+               :delete-url="delete_uri" :show-edit="false" :columns="columns">
+    </DataTable>
+
+  </div>
 </template>
 
 <script>
+const DataTable = () => import('../../../components/DataTable')
+
 export default {
-  name: "Course"
+  name: "list",
+  data() {
+    return {
+      rows: [
+        'id',
+        'title'
+      ],
+      edit_url_name: 'factor-course-edit',
+      delete_uri: 'factors',
+      columns: [
+        {
+          field: 'id',
+          label: 'شناسه',
+        },
+        {
+          field: 'title',
+          label: 'عنوان دوره'
+        },
+      ]
+    }
+  },
+  components: {
+    DataTable
+  },
+  mounted() {
+  }
+
 }
 </script>
 
