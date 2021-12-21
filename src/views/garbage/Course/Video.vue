11c3e834a483d67dde1f61890d1f71e97b8ee2e7
mohammad <mawebcoder@gmail.com>
2021-12-21 18:51:58 +0330

    trash videos list search doen

--- src/views/garbage/Course/Video.vue
+++ src/views/garbage/Course/Video.vue
@@ -1,12 +1,44 @@
 <template>
   <div>
-    video garbage
+
+    <DataTable :force-delete="true" restore-url="videos/restore" force-delete-url="videos/force/delete"
+               :showRestore="true" :isTrash="true"
+               server-search-route="videos/search-items?trash=true" :items="rows"
+              :show-edit="false" :columns="columns">
+    </DataTable>
+
   </div>
 </template>
 
 <script>
+const DataTable = () => import('../../../components/DataTable')
+
 export default {
-  name: "Video"
+  name: "list",
+  data() {
+    return {
+      rows: [
+        'id',
+        'title'
+      ],
+      columns: [
+        {
+          field: 'id',
+          label: 'شناسه',
+        },
+        {
+          field: 'title',
+          label: 'عنوان  ویدیو'
+        },
+      ]
+    }
+  },
+  components: {
+    DataTable
+  },
+  mounted() {
+  }
+
 }
 </script>
 
